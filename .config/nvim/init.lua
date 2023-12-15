06c0fbca8fddcf8cf420c86624b0406b5efb3fea
Josip Sikiric <josip.sikiric@proton.me>
2023-12-15 20:48:17 +0100

    fixed nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.wo.relativenumber = true
-vim.keymap.set('n', '<leader>e', '<Cmd>Explore<CR>', {})
-
-
---lazy.nvim config
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -21,55 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-  },
-}
 local opst = {}
 
-require("lazy").setup(plugins, opst)
-
-
---catppuccin.nvim theme config
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-
---telescope.nvim config
-local builtin = require("telescope.builtin")
-local themes = require('telescope.themes')
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-
---treesitter config
-local config = require("nvim-treesitter.configs")
-
-config.setup({
- ensure_installed = {"lua", "c", "python", "java", "cpp", "rust"},
- highlight = { enable = true },
- indent = { enable = true },
-})
-
-
---neo-tree.nvim config
-vim.keymap.set('n', '<leader>a', '<Cmd>Neotree toggle<CR>', {})
+require("vim-options")
+require("lazy").setup("plugins", opst)
