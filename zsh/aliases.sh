1800a98241deb3068ce9df0c792798636a692a8a
Josip Sikiric <josip.sikiric@proton.me>
2024-01-02 22:08:43 +0100

    got rid of oh-my-zsh

---
+++ zsh/aliases.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+
+alias zshconfig="nvim ~/zsh/.zshrc"
+alias cls='clear'
+alias ll='ls -la'
+alias nv='nvim'
+alias nf='neofetch'
+alias pf='pfetch'
