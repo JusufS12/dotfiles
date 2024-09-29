3eafdab4973effb5c25ec20915bce008ee5c5e1b
Josip Sikiric <josip.sikiric@proton.me>
2024-09-29 14:52:31 +0200

    changed zsh config

--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,1 +1,2 @@
 export ZDOTDIR=~/zsh/
+export LC_ALL=C.UTF-8
--- zsh/shell/aliases.sh
+++ zsh/shell/aliases.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/zsh
 
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-
-
-alias zshconfig="nvim ~/zsh/.zshrc"
+alias zshconfig='vim ~/zsh/.zshrc'
+alias zshreload='source ~/zsh/.zshrc'
 alias cls='clear'
 alias ll='ls -la'
+alias vim='nvim'
 alias nv='nvim'
 alias nf='neofetch'
 alias pf='pfetch'
+alias ff='fastfetch'
